10806b023011d70b57004873fa08a1f66e8e37bb
Tsuyoshi Ushio <ushio@simplearchitect.com>
2015-02-14 12:10:17 +0900

    Improve the usability of constructor

--- repository.rb
+++ repository.rb
@@ -13,16 +13,15 @@ class Repository
   private
   def self.load_yaml(path)
     config_path = File.expand_path("../#{path}/repository.yml", __FILE__)
-    environment = ENV['RAILS_ENV']
-    environment ||= 'development'
+
     begin
       f = open(config_path)
-      config_contents = YAML.load(f.read)
+      contents = YAML.load(f.read)
       f.close
     rescue Exception => e
        raise "#{config_path}にrepository.yamlが見つかりません"
     end
-    {:config_path => config_path, :environment => environment, :config_contents => config_contents}
+    {:config_path => config_path,  :contents => contents}
   end
 
   def self.generate_methods(config, environment)
@@ -33,8 +32,9 @@ class Repository
 
   def initialize(config)
     @config_path = config[:config_path]
-    @environment = config[:environment]
-    @config = config[:config_contents]
+    @environment = ENV['RAILS_ENV']
+    @environment ||= 'development'
+    @config = config[:contents]
     Repository.generate_methods(@config, @environment)
   end
 end
